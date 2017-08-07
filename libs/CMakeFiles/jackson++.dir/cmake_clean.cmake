1ccc768cb6522380bffc2bcc5f13d1a9b735dc28
adadesions <ada_dev@icloud.com>
2017-08-07 12:30:23 +0700

    Set up c++ library as jackson++

---
+++ libs/CMakeFiles/jackson++.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jackson++.dir/src/jackson.cpp.o"
+  "build/jackson++.pdb"
+  "build/jackson++"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jackson++.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
